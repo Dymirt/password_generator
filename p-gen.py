f8dec771a638f2c6d33168eefe7162937f188673
Dymirt <kolidadmytro@gmail.com>
2021-02-10 18:03:49 +0100

    Style update

--- p-gen.py
+++ p-gen.py
@@ -3,30 +3,33 @@
 def main():
     print("This program alous you to create your personal strong password based on e.g \"Your Name\" and its easy to remember.")
     while True:
-        password = inToString(generatePassword(input("Create password based on: ")))
+        password = inToString(generatePassword(
+            input("Create password based on: ")))
         if len(password) < 8:
             print("Your word is too short.")
-        else:     
-            print("Try this password:",password)
+        else:
+            print("Try this password:", password)
             break
-            
+
 # This function convert list in to string
-# 
+
+
 def inToString(l):
     s = ""
     for i in l:
         s += i
     return s
-    
+
 # This function create pasword based on input
-#
+
+
 def generatePassword(password):
     new_password = []
     for item in password:
         if item == " ":
             item = ""
         elif item == "a" or item == "A":
-            item = "@" 
+            item = "@"
         elif item == "l" or item == "L":
             item = "1"
         elif item == "e" or item == "E":
@@ -43,7 +46,9 @@ def generatePassword(password):
             item = item.upper()
         elif item.isupper():
             item = item.lower()
-        
+
         new_password.append(item)
     return new_password
+
+
 main()
