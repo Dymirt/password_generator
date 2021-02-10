0a8b4b17b73d2472ac803d83563b2fa119e002a6
Dymirt <kolidadmytro@gmail.com>
2021-02-10 17:18:02 +0100

    Added new features

--- p-gen.py
+++ p-gen.py
@@ -1,12 +1,15 @@
 # Main program
 #
 def main():
-    password = inToString(generatePassword(input("Create password based on word: ")))
-    if len(password) < 8:
-        print("Your word is too short.")
-    else:
-        print(password)
-
+    print("This program alous you to create your personal strong password based on e.g \"Your Name\" and its easy to remember.")
+    while True:
+        password = inToString(generatePassword(input("Create password based on: ")))
+        if len(password) < 8:
+            print("Your word is too short.")
+        else:     
+            print("Try this password:",password)
+            break
+            
 # This function convert list in to string
 # 
 def inToString(l):
@@ -14,21 +17,28 @@ def inToString(l):
     for i in l:
         s += i
     return s
+    
 # This function create pasword based on input
 #
 def generatePassword(password):
     new_password = []
     for item in password:
         if item == " ":
-            item = "" 
-        elif item == "l":
+            item = ""
+        elif item == "a" or item == "A":
+            item = "@" 
+        elif item == "l" or item == "L":
             item = "1"
         elif item == "e" or item == "E":
             item = "3"
+        elif item == "i" or item == "I":
+            item = "!"
         elif item == "s" or item == "S":
             item = "$"
         elif item == "o" or item == "O":
             item = "0"
+        elif item == "p" or item == "P":
+            item = "%"
         elif item.islower():
             item = item.upper()
         elif item.isupper():
